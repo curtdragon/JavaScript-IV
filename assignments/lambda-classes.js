dd4684a9e5aa69e0e7a4e6eb2aeb33dc9552353a
curtdragon <tealturboser@gmail.com>
2019-04-24 22:30:56 -0500

    Refactor done. Skeleton of lambda done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,43 @@
 // CODE here for your Lambda Classes
+
+
+class Person {
+    constructor(character) {
+        this.createdAt = character.createdAt;
+        this.name = character.name;
+        this.dimensions = character.dimensions;
+    }
+    destroy() {
+        return `${this.name} has been removed from the game.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(stats) {
+        super(stats);
+        this.healthPoints = stats.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
+
+class Student extends Instructor {
+    constructor(stats) {
+        super(stats);
+        this.healthPoints = stats.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
+
+class ProjectManager extends Student {
+    constructor(stats) {
+        super(stats);
+        this.healthPoints = stats.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
